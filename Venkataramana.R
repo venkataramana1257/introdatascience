befe5d5abdeb896b34ec73c09b5a9491807bbbe4
venkat_1257 <venkat@cedartech.com.my>
2021-11-19 01:24:55 +0800

    Part 2 For Tutorial 2

---
+++ Venkataramana.R
@@ -0,0 +1,15 @@
+library(rvest)
+library(dplyr)
+
+link = "https://www.imdb.com/search/title/?title_type=feature&num_votes=25000,&genres=adventure&sort=user_rating,desc"
+page = read_html(link)
+
+name = page %>% html_nodes(".lister-item-header a") %>% html_text()
+movie_links = page %>% html_nodes(".lister-item-header a") %>% html_attr("href") %>% paste("https://www.imdb.com", ., sep="")
+year = page %>% html_nodes(".text-muted.unbold") %>% html_text()
+rating = page %>% html_nodes(".ratings-imdb-rating strong") %>% html_text()
+synopsis = page %>% html_nodes(".ratings-bar+ .text-muted") %>% html_text()
+
+movies = data.frame(name, year, rating, synopsis, stringsAsFactors = FALSE)
+
+View(movies)
